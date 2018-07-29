59b545848bdc469801385ff5f62e46b7e2c80944
antonlunev <2hammer90@mail.ru>
2018-07-29 11:26:25 -0700

    update angular, add tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,34 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {environment} from '../environments/environment';
 
 import {AppComponent} from './app.component';
+import {TasksComponent} from './tasks/tasks.component';
+import { TaskComponent } from './tasks/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+
+    EffectsModule.forRoot([/*PostEffects*/]),
+
+    StoreModule.forRoot({
+      // post: postReducer
+    }),
+
+    StoreDevtoolsModule.instrument({maxAge: 25})
   ],
   providers: [],
   bootstrap: [AppComponent]
