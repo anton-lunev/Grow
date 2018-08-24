27582784d640e9fdb50ee547b3f78b8d918e6697
antonlunev <2hammer90@mail.ru>
2018-08-23 21:37:53 -0700

    first version of goal cards

--- src/app/layout/task-list/task-list.component.ts
+++ src/app/layout/task-list/task-list.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+// TODO rename it to GoalsListComponent
 @Component({
   selector: 'grow-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
-
+  goals = [
+    {
+      title: 'Listen book "Talk to crazy"',
+      description: 'Great book that makes some suggestions how to talk with psychopaths',
+      image: ''
+    },
+    {
+      title: 'Finish MVP of this app',
+      description: 'Minimal viable product is a product with just enough features to satisfy early customers',
+      image: ''
+    },
+    {
+      title: 'Learn Ngrx',
+      description: 'Watch Angular Ngrx course and apply knowledge to this app',
+      image: ''
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
