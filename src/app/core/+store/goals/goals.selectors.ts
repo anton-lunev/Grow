c9900cab250d438bfe124acc520fb17e471eeb16
antonlunev <2hammer90@mail.ru>
2018-09-16 22:45:44 -0700

    fix ngrx

--- src/app/core/+store/goals/goals.selectors.ts
+++ src/app/core/+store/goals/goals.selectors.ts
@@ -3,14 +3,13 @@ import { goalsAdapter, GoalsState } from './goals.state';
 
 export const getGoalsState = createFeatureSelector<GoalsState>('goals');
 
-export const getGoalsData = createSelector(getGoalsState, (state: GoalsState) => state.entities);
 export const getTasksError = createSelector(getGoalsState, (state: GoalsState) => state.error);
 export const getTasksLoaded = createSelector(getGoalsState, (state: GoalsState) => state.loaded);
 
-//
-// export const {
-//   selectEntities: getGoalsEntities,
-//   selectAll: getGoalsData
-// } = goalsAdapter.getSelectors(getGoalsState);
+
+export const {
+  selectEntities: getGoalsEntities,
+  selectAll: getGoalsData
+} = goalsAdapter.getSelectors(getGoalsState);
 
 
