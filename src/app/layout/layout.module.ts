252acdd58820e5b862b3ce340a4146a8665d03e9
antonlunev <2hammer90@mail.ru>
2018-11-05 21:32:54 -0800

    implement svg sprite and add icons to sidebar menu

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 import { SideNavComponent } from './sidebar/side-nav/side-nav.component';
 import { SideStatisticsComponent } from './sidebar/side-statistics/side-statistics.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { UserInfoComponent } from './sidebar/user-info/user-info.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SharedModule],
   declarations: [SidebarComponent, SideNavComponent, UserInfoComponent, SideStatisticsComponent],
   exports: [SidebarComponent]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ContenteditableDirective } from './directives/contenteditable.directive';
+import { IconComponent } from './icon/icon.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ContenteditableDirective],
-  exports: [ContenteditableDirective]
+  declarations: [ContenteditableDirective, IconComponent],
+  exports: [ContenteditableDirective, IconComponent]
 })
 export class SharedModule {
 }
